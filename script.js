d053bb63f73d706e0a8210dc51bafc6aad333bcb
its-miss-arii <arianna.m.rodriguez0809@gmail.com>
2025-12-12 08:20:33 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,144 @@
+// declare your variables
+
+//story function
+function littleShop() {
+  document.getElementById("storyHolder").innerHTML =
+    "Little Shop of Horrors is a musical and fil  m about a meek florist, Seymour, who finds a strange, man-eating plant that promises him fame and fortune if he feeds it blood. As the plant, named Audrey II, grows in size and appetite, Seymour is forced to commit increasingly desperate acts to keep it fed, leading to dark and comedic consequences for the shop and its inhabitants. The story explores themes of ambition, greed, and the consequences of one's choices.";
+  document.getElementById("click").play();
+  document.getElementById("my-shop").play();
+  document.getElementById("popular").pause();
+  document.getElementById("alex").pause();
+  document.getElementById("candy").pause();
+  document.getElementById("together").pause();
+  document.getElementById("spelling").pause();
+  document.getElementById("into-woods").pause();
+  document.getElementById("maybe").pause();
+  document.getElementById("chorus").pause();
+}
+function wicked() {
+  document.getElementById("storyHolder").innerHTML =
+    "Wicked is a prequel to The Wizard of Oz that tells the untold story of the Witches of Oz, focusing on the unlikely friendship between the green-skinned Elphaba and the popular blonde Galinda. The musical explores how their rivalry turns into a deep bond, how Elphaba becomes known as the Wicked Witch of the West, and how the two are ultimately driven to opposite sides of a conflict in Oz that begins when Elphaba discovers the Wizard is not what he seems.  ";
+  document.getElementById("click").play();
+  document.getElementById("popular").play();
+  document.getElementById("my-shop").pause();
+  document.getElementById("alex").pause();
+  document.getElementById("candy").pause();
+  document.getElementById("together").pause();
+  document.getElementById("spelling").pause();
+  document.getElementById("into-woods").pause();
+  document.getElementById("maybe").pause();
+  document.getElementById("chorus").pause();
+}
+function hamilton() {
+  document.getElementById("storyHolder").innerHTML =
+    "Hamilton is a musical that chronicles the life of American Founding Father Alexander Hamilton, from his arrival in America as an orphaned immigrant to his role as George Washington's right-hand man during the Revolutionary War and his eventual death in a duel with Aaron Burr. The show uses hip-hop, R&B, and traditional show tunes to tell the story of his rise, his political achievements, marriage to Eliza Schuyler, and complex relationship with Burr.";
+  document.getElementById("click").play();
+  document.getElementById("alex").play();
+  document.getElementById("popular").pause();
+  document.getElementById("my-shop").pause();
+  document.getElementById("candy").pause();
+  document.getElementById("together").pause();
+  document.getElementById("spelling").pause();
+  document.getElementById("into-woods").pause();
+  document.getElementById("maybe").pause();
+  document.getElementById("chorus").pause();
+}
+function heathers() {
+  document.getElementById("storyHolder").innerHTML =
+    "Heathers: The Musical is a darkly comedic musical adaptation of the 1989 movie, about a brainy misfit named Veronica Sawyer who tries to navigate the popular clique of the Heathers at Westerberg High. When she falls for the mysterious new kid, J.D., their dislike for the Heathers escalates into a cycle of revenge and murder. The show uses a heightened 1980s setting to satirize high school life and explore mature themes like bullying, suicide, and violence.  ";
+  document.getElementById("click").play();
+  document.getElementById("candy").play();
+  document.getElementById("popular").pause();
+  document.getElementById("my-shop").pause();
+  document.getElementById("alex").pause();
+  document.getElementById("together").pause();
+  document.getElementById("spelling").pause();
+  document.getElementById("into-woods").pause();
+  document.getElementById("maybe").pause();
+  document.getElementById("chorus").pause();
+}
+function grease() {
+  document.getElementById("storyHolder").innerHTML =
+    "Grease is a 1978 musical film, based on a 1971 stage musical, that follows the romance between a greaser named Danny Zuko and an Australian girl named Sandy Olsson at Rydell High in 1959. After a summer fling, they discover Sandy has transferred to his school, and the plot explores their on-again, off-again relationship and attempts to navigate high school, friendship, and social pressures, culminating in Sandy embracing a new, more rebellious persona to win Danny over. ";
+  document.getElementById("click").play();
+  document.getElementById("together").play();
+  document.getElementById("popular").pause();
+  document.getElementById("my-shop").pause();
+  document.getElementById("alex").pause();
+  document.getElementById("candy").pause();
+  document.getElementById("spelling").pause();
+  document.getElementById("into-woods").pause();
+  document.getElementById("maybe").pause();
+  document.getElementById("chorus").pause();
+}
+function spellingBee() {
+  document.getElementById("storyHolder").innerHTML =
+    "The 25th Annual Putnam County Spelling Bee is a musical comedy about six young, eclectic students who compete in a spelling bee, where they share personal stories and learn about life. An unusual feature is that four audience members are invited on stage to participate, adding an element of improvisation and unpredictability to every show. The musical is known for being a hilarious, fast-paced, and touching crowd-pleaser that has won Tony and Drama Desk awards for its book. ";
+  document.getElementById("click").play();
+  document.getElementById("spelling").play();
+  document.getElementById("popular").pause();
+  document.getElementById("my-shop").pause();
+  document.getElementById("alex").pause();
+  document.getElementById("candy").pause();
+  document.getElementById("together").pause();
+  document.getElementById("into-woods").pause();
+  document.getElementById("maybe").pause();
+  document.getElementById("chorus").pause();
+}
+function intoTheWoods() {
+  document.getElementById("storyHolder").innerHTML =
+    "Into the Woods is a musical that intertwines classic fairy tales like Cinderella, Little Red Riding Hood, Jack and the Beanstalk, and Rapunzel, focusing on a childless baker and his wife who must collect magical items for a witch to reverse their curse. By the end of the first act, each character achieves their happily ever after, but the second act explores the disastrous consequences of their wishes, forcing them to come together to survive a giant and learn that wishes have a price. The show is a dark comedy with a moral lesson about the unintended consequences of getting what you wish for. ";
+  document.getElementById("click").play();
+  document.getElementById("into-woods").play();
+  document.getElementById("popular").pause();
+  document.getElementById("my-shop").pause();
+  document.getElementById("alex").pause();
+  document.getElementById("candy").pause();
+  document.getElementById("together").pause();
+  document.getElementById("spelling").pause();
+  document.getElementById("maybe").pause();
+  document.getElementById("chorus").pause();
+}
+function cabaret() {
+  document.getElementById("storyHolder").innerHTML =
+    "Cabaret is a musical set in 1930s Berlin that follows American writer Cliff Bradshaw as he becomes entangled with English cabaret singer Sally Bowles at the decadent Kit Kat Klub. While the duo pursues their relationship, their story unfolds against the backdrop of the growing Nazi Party's rise to power. A subplot follows the romance between a German boarding house owner and a Jewish fruit vendor, and the show explores themes of escapism, decadence, and the consequences of ignoring political unrest.  ";
+  document.getElementById("click").play();
+  document.getElementById("maybe").play();
+  document.getElementById("popular").pause();
+  document.getElementById("my-shop").pause();
+  document.getElementById("alex").pause();
+  document.getElementById("candy").pause();
+  document.getElementById("together").pause();
+  document.getElementById("spelling").pause();
+  document.getElementById("into-woods").pause();
+  document.getElementById("chorus").pause();
+}
+function chorusLine() {
+  document.getElementById("storyHolder").innerHTML =
+    "A Chorus Line is a musical set during a grueling Broadway dance audition where a director, Zach, forces sixteen dancers to reveal their personal stories and the life-changing events that led them to pursue a career in dance. The show explores the inner lives, ambitions, and struggles of these performers, culminating in the selection of the final cast for the show. Key themes include the bittersweet nature of their dreams, the competitive and often brutal audition process, and the dedication required for a career in show business. ";
+  document.getElementById("click").play();
+  document.getElementById("chorus").play();
+  document.getElementById("popular").pause();
+  document.getElementById("my-shop").pause();
+  document.getElementById("alex").pause();
+  document.getElementById("candy").pause();
+  document.getElementById("together").pause();
+  document.getElementById("spelling").pause();
+  document.getElementById("into-woods").pause();
+  document.getElementById("maybe").pause();
+}
+
+function resetAll() {
+  document.getElementById("storyHolder").innerHTML =
+    "Welcome, and Thank You, for checking out Musical Catalog! Here are some fun musicals if you are just getting into theatre. Click on the cover image and read description of each. ";
+  document.getElementById("click").play();
+  document.getElementById("chorus").pause();
+  document.getElementById("popular").pause();
+  document.getElementById("my-shop").pause();
+  document.getElementById("alex").pause();
+  document.getElementById("candy").pause();
+  document.getElementById("together").pause();
+  document.getElementById("spelling").pause();
+  document.getElementById("into-woods").pause();
+  document.getElementById("maybe").pause();
+}
